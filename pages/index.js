4bf3e88ee7bd47a1c34181c60ec57f6da8bdaf8f
Carolina Parisi <carolinaparisi5@gmail.com>
2024-08-21 14:16:52 +0100

    add pages directory

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home() {
+	return (
+		<h1>
+			Parabéns por conseguir cada vez mais! Seu esforço, dedicação e carinho
+			sempre serão recomenpensados 😊 Tenho muito orgulho de tu!
+		</h1>
+	);
+}
+
+export default Home;
