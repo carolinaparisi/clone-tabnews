83d7e4b0a94056830faa37bbb0edc53a54ee830a
Carolina Parisi <carolinaparisi5@gmail.com>
2025-01-07 16:07:06 +0000

    finish /api/v1/status endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,30 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum");
-  console.log(result);
-  response.status(200).json("alunos do curso.dev são pessoas acima da média");
+  const updatedAt = new Date().toISOString();
+
+  const databaseName = process.env.POSTGRES_DB;
+  const usedConn = await database.query({
+    text: `SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;`,
+    values: [databaseName],
+  });
+  const maxConn = await database.query(`SHOW max_connections;`);
+  const databaseVers = await database.query(`SHOW server_version;`);
+
+  const usedConnections = usedConn.rows[0].count;
+  const maxConnections = maxConn.rows[0].max_connections;
+  const databaseVersion = databaseVers.rows[0].server_version;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersion,
+        max_connections: parseInt(maxConnections),
+        opened_connections: usedConnections,
+      },
+    },
+  });
 }
 
 export default status;
