c25b2ac0d283c6fb4a8ef0bee4d4fcc3cac914ec
Carolina Parisi <carolinaparisi5@gmail.com>
2025-01-13 23:19:29 +0000

    add .env.development into gitignore and add ssl property

--- infra/database.js
+++ infra/database.js
@@ -7,15 +7,16 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: true,
   });
 
-  console.log({
+  /*   console.log({
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-  });
+  }); */
 
   try {
     await client.connect();
